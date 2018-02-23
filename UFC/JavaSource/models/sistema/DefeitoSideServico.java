e817506ca9e56ef9f19224c8db1e18f1603f8346
FabioClem <fabio.henrique.clem@gmail.com>
2018-02-23 09:29:16 -0300

    Correção de erro no CSS e modificado parametro data do relatorio para data de ação.

--- UFC/JavaSource/models/sistema/DefeitoSideServico.java
+++ UFC/JavaSource/models/sistema/DefeitoSideServico.java
@@ -19,264 +19,263 @@ import entidades.sistema.Tipificacao;
 @Stateless
 public class DefeitoSideServico {
 
-	@PersistenceContext(unitName = "vu")
-	private EntityManager entityManager;
+    @PersistenceContext(unitName = "vu")
+    private EntityManager entityManager;
 
-	public void cadastrarDefeitoSide(DefeitoSide defeitoSide, UsuarioEfika usuarioEfika) throws Exception {
+    public void cadastrarDefeitoSide(DefeitoSide defeitoSide, UsuarioEfika usuarioEfika) throws Exception {
 
-		try {
+        try {
 
-			defeitoSide.setUsuarioEfika(usuarioEfika);
+            defeitoSide.setUsuarioEfika(usuarioEfika);
 
-			this.entityManager.persist(defeitoSide);
+            this.entityManager.persist(defeitoSide);
 
-		} catch (Exception e) {
+        } catch (Exception e) {
 
-			throw new Exception("Erro ao cadastrar Defeito Side");
+            throw new Exception("Erro ao cadastrar Defeito Side");
 
-		}
+        }
 
-	}
+    }
 
-	public void modificarDefeitoSide(DefeitoSide defeitoSide, StatusDefeito statusDefeito) throws Exception {
+    public void modificarDefeitoSide(DefeitoSide defeitoSide, StatusDefeito statusDefeito) throws Exception {
 
-		try {
-			
-			Date date = new Date();
-			
-			defeitoSide.setDataAcao(date);
-			
-			defeitoSide.setStatusDefeito(statusDefeito);
+        try {
 
-			this.entityManager.merge(defeitoSide);
+            Date date = new Date();
 
-		} catch (Exception e) {
+            defeitoSide.setDataAcao(date);
 
-			throw new Exception("Erro ao modificar Defeito Side");
+            defeitoSide.setStatusDefeito(statusDefeito);
 
-		}
+            this.entityManager.merge(defeitoSide);
 
-	}
+        } catch (Exception e) {
 
-	@SuppressWarnings("unchecked")
-	public List<DefeitoSide> listarDefeitoSide(StatusDefeito statusDefeito) {
+            throw new Exception("Erro ao modificar Defeito Side");
 
-		try {
+        }
 
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.statusDefeito =:param1 ORDER BY d.dataIntegrado DESC");
-			query.setParameter("param1", statusDefeito);
-			return query.getResultList();			
+    }
 
-		} catch (Exception e) {
+    @SuppressWarnings("unchecked")
+    public List<DefeitoSide> listarDefeitoSide(StatusDefeito statusDefeito) {
 
-			return new ArrayList<DefeitoSide>();
+        try {
 
-		}
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.statusDefeito =:param1 ORDER BY d.dataIntegrado DESC");
+            query.setParameter("param1", statusDefeito);
+            return query.getResultList();
 
-	}
+        } catch (Exception e) {
 
-	public DefeitoSide listarDefeitoSideEspecifico(DefeitoSide defeitoSide, UsuarioEfika usuarioEfika, StatusDefeito statusDefeito) throws Exception {
+            return new ArrayList<DefeitoSide>();
 
-		try {
+        }
 
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.id =:param1 AND d.usuarioEfika =:param2 AND d.statusDefeito =:param3");
-			query.setParameter("param1", defeitoSide.getId());
-			query.setParameter("param2", usuarioEfika);
-			query.setParameter("param3", statusDefeito);
-			return (DefeitoSide) query.getSingleResult();
+    }
 
-		} catch (Exception e) {
+    public DefeitoSide listarDefeitoSideEspecifico(DefeitoSide defeitoSide, UsuarioEfika usuarioEfika, StatusDefeito statusDefeito) throws Exception {
 
-			throw new Exception("Defeito Side não existe ou nao está associado a sua matricula.");
+        try {
 
-		}
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.id =:param1 AND d.usuarioEfika =:param2 AND d.statusDefeito =:param3");
+            query.setParameter("param1", defeitoSide.getId());
+            query.setParameter("param2", usuarioEfika);
+            query.setParameter("param3", statusDefeito);
+            return (DefeitoSide) query.getSingleResult();
 
-	}
+        } catch (Exception e) {
 
-	public DefeitoSide listarDefeitoSideEspecifico(DefeitoSide defeitoSide) throws Exception {
+            throw new Exception("Defeito Side não existe ou nao está associado a sua matricula.");
 
-		try {
+        }
 
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.id =:param1");
-			query.setParameter("param1", defeitoSide.getId());
-			return (DefeitoSide) query.getSingleResult();
+    }
 
-		} catch (Exception e) {
+    public DefeitoSide listarDefeitoSideEspecifico(DefeitoSide defeitoSide) throws Exception {
 
-			throw new Exception("Defeito Side não existe ou nao está associado a sua matricula.");
+        try {
 
-		}
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.id =:param1");
+            query.setParameter("param1", defeitoSide.getId());
+            return (DefeitoSide) query.getSingleResult();
 
-	}
+        } catch (Exception e) {
 
-	
-	public void assumirDefeitoSide(DefeitoSide defeitoSide, StatusDefeito statusDefeito, UsuarioEfika usuarioEfika) throws Exception {
+            throw new Exception("Defeito Side não existe ou nao está associado a sua matricula.");
 
-		try {
+        }
 
-			defeitoSide.setStatusDefeito(statusDefeito);
-			defeitoSide.setUsuarioEfika(usuarioEfika);
+    }
 
-			this.entityManager.merge(defeitoSide);
+    public void assumirDefeitoSide(DefeitoSide defeitoSide, StatusDefeito statusDefeito, UsuarioEfika usuarioEfika) throws Exception {
 
-		} catch (Exception e) {
+        try {
 
-			throw new Exception("Erro ao assumir Defeito");
+            defeitoSide.setStatusDefeito(statusDefeito);
+            defeitoSide.setUsuarioEfika(usuarioEfika);
 
-		}		
+            this.entityManager.merge(defeitoSide);
 
-	}
+        } catch (Exception e) {
 
-	@SuppressWarnings("unchecked")
-	public List<DefeitoSide> listarMeusDefeitos(UsuarioEfika usuarioEfika, StatusDefeito statusDefeito) {
+            throw new Exception("Erro ao assumir Defeito");
 
-		try {
+        }
 
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.usuarioEfika =:param1 AND d.statusDefeito =:param2");
-			query.setParameter("param1", usuarioEfika);
-			query.setParameter("param2", statusDefeito);
-			return query.getResultList();
+    }
 
-		} catch (Exception e) {
+    @SuppressWarnings("unchecked")
+    public List<DefeitoSide> listarMeusDefeitos(UsuarioEfika usuarioEfika, StatusDefeito statusDefeito) {
 
-			return new ArrayList<DefeitoSide>();
+        try {
 
-		}
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.usuarioEfika =:param1 AND d.statusDefeito =:param2");
+            query.setParameter("param1", usuarioEfika);
+            query.setParameter("param2", statusDefeito);
+            return query.getResultList();
 
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<DefeitoSide> listarDefeitoSideEncerradoSemSS(StatusDefeito statusDefeito) {
-		
-		try {
-			
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.statusDefeito =:param1 AND d.ss = ''");
-			query.setParameter("param1", statusDefeito);
-			return query.getResultList();
-			
-		} catch (Exception e) {
-			
-			return new ArrayList<DefeitoSide>();
-			
-		}
-		
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<DefeitoSide> listarDefeitoSideEncerrado(StatusDefeito statusDefeito, Date dataInicio, Date dataFim) {
-		
-		try {
-			
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.statusDefeito =:param1 AND d.dataIntegrado BETWEEN :param2 AND :param3");
-			query.setParameter("param1", statusDefeito);
-			query.setParameter("param2", dataInicio);
-			query.setParameter("param3", dataFim);
-			return query.getResultList();
-			
-		} catch (Exception e) {
-			
-			return new ArrayList<DefeitoSide>();
-			
-		}
-		
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<DefeitoSide> listarDefeitoSideMotivoEncerramento(MotivoEncerramentoDefeitoSide motivoEncerramento) {
-		
-		try {
-			
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.motivoEncerramento =:param1 AND d.dataIntegrado > CURRENT_DATE");
-			query.setParameter("param1", motivoEncerramento);
-			return query.getResultList();			
-			
-		} catch (Exception e) {
-			
-			return new ArrayList<DefeitoSide>();			
+        } catch (Exception e) {
 
-		}
-		
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<DefeitoSide> listarDefeitoSideStatus(StatusDefeito statusDefeito) {
+            return new ArrayList<DefeitoSide>();
 
-		try {
-			
-			String queryC = "";
-			
-			if (statusDefeito.getNome().equalsIgnoreCase("Encerrado") || statusDefeito.getNome().equalsIgnoreCase("Removido Fila")) {				
-				
-				queryC = " AND d.dataAcao > CURRENT_DATE";
-				
-			}else{
-				
-				queryC = "AND d.dataIntegrado > CURRENT_DATE";
-				
-			}
-			
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.statusDefeito =:param1" + queryC);			
-			query.setParameter("param1", statusDefeito);
-			return query.getResultList();
+        }
 
-		} catch (Exception e) {
+    }
 
-			return new ArrayList<DefeitoSide>();
+    @SuppressWarnings("unchecked")
+    public List<DefeitoSide> listarDefeitoSideEncerradoSemSS(StatusDefeito statusDefeito) {
 
-		}
+        try {
 
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<DefeitoSide> listarDefeitoSideTipificacao(Tipificacao tipificacao) {
-		
-		try {
-						
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.tipificacao =:param1 AND d.dataIntegrado > CURRENT_DATE");
-			query.setParameter("param1", tipificacao);
-			return query.getResultList();
-			
-		} catch (Exception e) {
-			
-			return new ArrayList<DefeitoSide>();
-			
-		}
-		
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<DefeitoSide> listarDefeitoSideDeLoteEspecifico(Lote lote, StatusDefeito statusDefeito) {
-		
-		try {
-			
-			Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.lote =:param1 AND d.statusDefeito =:param2");
-			query.setParameter("param1", lote);
-			query.setParameter("param2", statusDefeito);
-			return query.getResultList();
-			
-		} catch (Exception e) {
-			
-			return new ArrayList<DefeitoSide>();
-			
-		}
-		
-	}
-	
-	@SuppressWarnings("unchecked")
-	public List<String> listarDefeitoSideDistinc(StatusDefeito statusDefeito) {
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.statusDefeito =:param1 AND d.ss = ''");
+            query.setParameter("param1", statusDefeito);
+            return query.getResultList();
 
-		try {
+        } catch (Exception e) {
 
-			Query query = this.entityManager.createQuery("SELECT DISTINCT d.lote.nome FROM DefeitoSide d WHERE d.statusDefeito =:param1");
-			query.setParameter("param1", statusDefeito);
-			return query.getResultList();			
+            return new ArrayList<DefeitoSide>();
 
-		} catch (Exception e) {
+        }
 
-			return new ArrayList<String>();
+    }
 
-		}
+    @SuppressWarnings("unchecked")
+    public List<DefeitoSide> listarDefeitoSideEncerrado(StatusDefeito statusDefeito, Date dataInicio, Date dataFim) {
 
-	}
-	
+        try {
+
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.statusDefeito =:param1 AND d.dataAcao BETWEEN :param2 AND :param3");
+            query.setParameter("param1", statusDefeito);
+            query.setParameter("param2", dataInicio);
+            query.setParameter("param3", dataFim);
+            return query.getResultList();
+
+        } catch (Exception e) {
+
+            return new ArrayList<DefeitoSide>();
+
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<DefeitoSide> listarDefeitoSideMotivoEncerramento(MotivoEncerramentoDefeitoSide motivoEncerramento) {
+
+        try {
+
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.motivoEncerramento =:param1 AND d.dataIntegrado > CURRENT_DATE");
+            query.setParameter("param1", motivoEncerramento);
+            return query.getResultList();
+
+        } catch (Exception e) {
+
+            return new ArrayList<DefeitoSide>();
+
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<DefeitoSide> listarDefeitoSideStatus(StatusDefeito statusDefeito) {
+
+        try {
+
+            String queryC = "";
+
+            if (statusDefeito.getNome().equalsIgnoreCase("Encerrado") || statusDefeito.getNome().equalsIgnoreCase("Removido Fila")) {
+
+                queryC = " AND d.dataAcao > CURRENT_DATE";
+
+            } else {
+
+                queryC = "AND d.dataIntegrado > CURRENT_DATE";
+
+            }
+
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.statusDefeito =:param1" + queryC);
+            query.setParameter("param1", statusDefeito);
+            return query.getResultList();
+
+        } catch (Exception e) {
+
+            return new ArrayList<DefeitoSide>();
+
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<DefeitoSide> listarDefeitoSideTipificacao(Tipificacao tipificacao) {
+
+        try {
+
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.tipificacao =:param1 AND d.dataIntegrado > CURRENT_DATE");
+            query.setParameter("param1", tipificacao);
+            return query.getResultList();
+
+        } catch (Exception e) {
+
+            return new ArrayList<DefeitoSide>();
+
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<DefeitoSide> listarDefeitoSideDeLoteEspecifico(Lote lote, StatusDefeito statusDefeito) {
+
+        try {
+
+            Query query = this.entityManager.createQuery("FROM DefeitoSide d WHERE d.lote =:param1 AND d.statusDefeito =:param2");
+            query.setParameter("param1", lote);
+            query.setParameter("param2", statusDefeito);
+            return query.getResultList();
+
+        } catch (Exception e) {
+
+            return new ArrayList<DefeitoSide>();
+
+        }
+
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<String> listarDefeitoSideDistinc(StatusDefeito statusDefeito) {
+
+        try {
+
+            Query query = this.entityManager.createQuery("SELECT DISTINCT d.lote.nome FROM DefeitoSide d WHERE d.statusDefeito =:param1");
+            query.setParameter("param1", statusDefeito);
+            return query.getResultList();
+
+        } catch (Exception e) {
+
+            return new ArrayList<String>();
+
+        }
+
+    }
+
 }
